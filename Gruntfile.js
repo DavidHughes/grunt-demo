943024b159fc2bf1ea39f9c8c1d851b1ff9abbb2
David Hughes <david.hughes125@gmail.com>
2014-04-21 23:27:56 +0100

    Comments are great.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,22 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    // Pulls in project metadata which can be used in other tasks.
     pkg: grunt.file.readJSON('package.json'),
+    // Checks for common JS mistakes. Rules configured by jshintrc file
     jshint: {
       files: ['Gruntfile.js'],
       options: {
         reporter: require('jshint-stylish')
       }
     },
+    // grunt-shell lets us use shell tasks for things we can't find native plugins for
     shell: {
       mdPress: {
         command: 'mdpress grunt-demo.md'
       }
     },
+    // Set up a webserver, this is just for livereload so that you can
+    // save grunt-demo.md and have your browser refresh automatically.
     express: {
       all: {
         options: {
@@ -34,11 +39,13 @@ module.exports = function(grunt) {
     }
   });
 
+  // Need to load the plugins npm has installed so Grunt can use them
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-express');
   grunt.loadNpmTasks('grunt-shell');
 
+  // As well as the above tasks, can create composite tasks here.
   grunt.registerTask('default', ['jshint', 'shell']);
   grunt.registerTask('develop', ['express', 'watch']);
 };
